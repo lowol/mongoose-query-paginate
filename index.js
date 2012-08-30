ee05ade8b0f7d21c39e22178d9ce656b0b7df957
lowol <@lowol>
2012-08-30 22:37:30 +0900

    options.offset

--- index.js
+++ index.js
@@ -12,26 +12,31 @@ Query.prototype.paginate = function(options, callback) {
   var defaults = {
     perPage: 10, // Number of items to display on each page.
     delta  :  5, // Number of page numbers to display before and after the current one.
-    page   :  1  // Initial page number.
+    page   :  1, // Initial page number.
+    offset :  0  // Offset number.
   };
 
   options = options || defaults;
   options.perPage = options.perPage || defaults.perPage;
   options.delta = options.delta || defaults.delta;
   options.page = options.page || defaults.page;
+  options.offset = options.offset || defaults.offset;
 
   var query = this;
   var model = query.model;
   model.count(query._conditions, function(err, count) {
-    query.skip((options.page - 1) * options.perPage).limit(options.perPage).execFind(function(err, results) {
+    var _skip = (options.page - 1) * options.perPage;
+    _skip += options.offset;
+    query.skip(_skip).limit(options.perPage).execFind(function(err, results) {
       if (err) {
         callback(err, {});
         return;
       }
 
+      results = results || [];
       var page = ~~options.page || 0;
       var delta = options.delta;
-      var last = Math.ceil(count / options.perPage);
+      var last = Math.ceil(count / (options.perPage + options.offset));
       var current = page;
       var start = page - delta > 1 ? page - delta : 1;
       var end = current + delta + 1 < last ? current + delta : last;
--- test/test.js
+++ test/test.js
@@ -90,6 +90,20 @@ describe('paginate', function() {
     });
   });
 
+  it('options.offset', function(done) {
+    Comment.find().paginate({offset: 2}, function(err, pager) {
+      assert.equal(pager.results[0].number, 3);
+      done(err);
+    });
+  });
+
+  it('options.offset & page', function(done) {
+    Comment.find().paginate({offset: 2, page: 2, perPage: 5}, function(err, pager) {
+      assert.equal(pager.results[0].number, 8);
+      done(err);
+    });
+  });
+
   it('custom query', function(done) {
     var query = Comment.find().where('number').lte(50).sort('number', -1);
     query.paginate({}, function(err, pager) {
