7e69be22491fab21d80422fefbb3a625b03577be
Mosly <moritz.wenko@gmx.net>
2025-02-07 06:54:47 +0000

    chore: upgrade tests to async await

--- test/test2.js
+++ test/test2.js
@@ -2,65 +2,60 @@
  * test
  */
 //var should = require('should');
-var assert = require('assert');
+var assert = require("assert");
 
-var mongoose = require('mongoose');
-require('../index');
+var mongoose = require("mongoose");
+require("../index");
 
-var conn = mongoose.createConnection('mongodb://localhost/query-test');
-//conn.db.dropDatabase();
+describe("paginate", function () {
+  let Comment;
+  let conn;
+  before(async function () {
+    conn = await mongoose.connect("mongodb://localhost/query-test");
 
-var CommentSchema = new mongoose.Schema({
-  number: Number,
-  name: String,
-  body: String
-});
-var Comment = conn.model('Comment', CommentSchema);
-
-describe('paginate', function() {
-
-  before(function(done) {
-    Comment.remove({}, function(err) {
-      var promises = [];
-      for (var i = 1; i <= 23; i++) {
-        promises.push(insert(i));
-      }
-
-      Promise.all(promises).then(function() {
-        done();
-      }, function(err) {
-        done(err);
-      });
+    const CommentSchema = new mongoose.Schema({
+      number: Number,
+      name: String,
+      body: String,
     });
+    Comment = conn.model("Comment", CommentSchema);
+    await Comment.deleteMany({});
+    const promises = [];
+    for (let i = 1; i <= 23; i++) {
+      promises.push(insert(i));
+    }
+
+    await Promise.all(promises);
 
     function insert(i) {
       return new Comment({
         number: i,
-        name: 'name' + i,
-        body: 'body' + i
+        name: "name" + i,
+        body: "body" + i,
       }).save();
     }
   });
 
-  after(function() {
-    conn.close();
+  after(async function () {
+    await conn.disconnect();
   });
 
- it('options.offset & page', function(done) {
-    Comment.find().paginate({offset: 2, perPage: 5, page: 5}, function(err, pager) {
-      assert.equal(pager.count, 23);
-      assert.equal(pager.last, 5);
-      done(err);
+  it("options.offset & page", async function () {
+    const pager = await Comment.find().paginate({
+      offset: 2,
+      perPage: 5,
+      page: 5,
     });
+    assert.equal(pager.count, 23);
+    assert.equal(pager.last, 5);
   });
-
- it('large options.offset', function(done) {
-    Comment.find().paginate({offset: 30, perPage: 5, page: 2}, function(err, pager) {
-      assert.equal(pager.count, 23);
-      assert.equal(pager.last, null);
-      done(err);
+  it("large options.offset", async function () {
+    const pager = await Comment.find().paginate({
+      offset: 30,
+      perPage: 5,
+      page: 2,
     });
+    assert.equal(pager.count, 23);
+    assert.equal(pager.last, null);
   });
-
-
 });
