2de60e049f1a9efb5578c080bcd555ecde3efe8f
Mosly <moritz.wenko@gmx.net>
2025-02-07 06:39:10 +0000

    chore(breaking): use async-await
    mongoose callback style is deprecated and will be removed

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /**
  * mongoose-query-paginate
  */
-var Query = require('mongoose').Query;
+var Query = require("mongoose").Query;
 
 /**
  * paginate
  *
  * @param {Object} options
  */
-Query.prototype.paginate = function(options, callback) {
-  var defaults = {
+Query.prototype.paginate = async function (options) {
+  const defaults = {
     perPage: 10, // Number of items to display on each page.
-    delta  :  5, // Number of page numbers to display before and after the current one.
-    page   :  1, // Initial page number.
-    offset :  0  // Offset number.
+    delta: 5, // Number of page numbers to display before and after the current one.
+    page: 1, // Initial page number.
+    offset: 0, // Offset number.
   };
 
   options = options || defaults;
@@ -22,49 +22,42 @@ Query.prototype.paginate = function(options, callback) {
   options.page = options.page || defaults.page;
   options.offset = options.offset || defaults.offset;
 
-  var query = this;
-  var model = query.model;
-  model.count(query._conditions, function(err, count) {
-    var _skip = (options.page - 1) * options.perPage;
-    _skip += options.offset;
-    query.skip(_skip).limit(+options.perPage).exec(function(err, results) {
-      if (err) {
-        callback(err, {});
-        return;
-      }
+  const query = this;
+  const model = query.model;
 
-      results = results || [];
-      var page = parseInt(options.page, 10) || 0;
-      var delta = options.delta;
-      var offset_count = count - options.offset;
-      offset_count = offset_count > 0 ? offset_count : 0;
-      var last = Math.ceil(offset_count / options.perPage);
-      var current = page;
-      var start = page - delta > 1 ? page - delta : 1;
-      var end = current + delta + 1 < last ? current + delta : last;
+  const count = await model.count(query._conditions);
+  const _skip = (options.page - 1) * options.perPage + options.offset;
+  const results = await query.skip(_skip).limit(+options.perPage).exec();
 
-      var pages = [];
-      for (var i = start; i <= end; i++) {
-        pages.push(i);
-      }
+  results = results || [];
+  const page = parseInt(options.page, 10) || 0;
+  const delta = options.delta;
+  let offset_count = count - options.offset;
+  offset_count = offset_count > 0 ? offset_count : 0;
+  const last = Math.ceil(offset_count / options.perPage);
+  const current = page;
+  const start = page - delta > 1 ? page - delta : 1;
+  const end = current + delta + 1 < last ? current + delta : last;
 
-      var prev = !count || current == start ? null : current - 1;
-      var next = !count || current == end ? null : current + 1;
-      if (!offset_count) {
-        prev = next = last = null;
-      }
+  const pages = [];
+  for (let i = start; i <= end; i++) {
+    pages.push(i);
+  }
 
-      var pager = {
-        'results': results,
-        'options': options,
-        'current': current,
-        'last': last,
-        'prev': prev,
-        'next': next,
-        'pages': pages,
-        'count': count
-      };
-      callback(err, pager);
-    });
-  });
+  const prev = !count || current == start ? null : current - 1;
+  const next = !count || current == end ? null : current + 1;
+  if (!offset_count) {
+    prev = next = last = null;
+  }
+
+  return {
+    results: results,
+    options,
+    current,
+    last,
+    prev,
+    next,
+    pages,
+    count,
+  };
 };
